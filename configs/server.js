f8469fe60150de4188eead9b585b78975ca61ec1
sanchez2021387 <jsanchez2021387@gmail.com>
2024-03-01 14:53:09 -0600

    Agregando todo a m pc

---
+++ configs/server.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { dbConnection } from './mongo.js';
+import userRoutes from '../src/user/user.routes.js';
+
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuarioPath = '/opinionManager/v1/users';
+
+        this.middlewares();
+        this.conectarDB();
+        this.routes();
+    }
+    async conectarDB() {
+        await dbConnection();
+    }
+
+    middlewares() {
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+
+    routes() {
+        this.app.use(this.usuarioPath, userRoutes);
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server running on port ', this.port);
+        });
+    }
+}
+
+export default Server;
